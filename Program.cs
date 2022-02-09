79b51c59b373abca5e15137dd8cd2953125ba8c5
simonnov007@gmail.com <simonnov007@gmail.com>
2022-02-09 13:50:37 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KedvencAllatom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Allat = Console.ReadLine();
+            Console.WriteLine();
+        }
+    }
+}
